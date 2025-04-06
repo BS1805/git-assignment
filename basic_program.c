b41b05d18704857a0c8049135531139f3f3ae999
Hasibul Hasan Hasib <hasib.hasan@brainstation-23.com>
2025-04-06 11:48:40 +0600

    Save my progress before rebase

--- basic_program.c
+++ basic_program.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int nth_prime(int n) {
+    int count = 0;
+    int num = 2;
+    while (count < n) {
+        if (is_prime(num)) {
+            count++;
+        }
+        num++;
+    }
+    return num - 1;
+}
+
 int factorial(int n) {
     if (n == 0) {
         return 1;
