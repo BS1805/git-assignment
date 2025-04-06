2811709849d5c133b51ee869d3324efdafe3c423
Hasibul Hasan Hasib <hasib.hasan@brainstation-23.com>
2025-04-06 11:34:53 +0600

    Save changes before rebase

--- basic_program.c
+++ basic_program.c
@@ -8,8 +8,21 @@ int factorial(int n) {
     }
 }
 
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
-    int num = 5;  // Change this value to test with different numbers
+    int num = 5;  // Factorial calculation
     printf("Factorial of %d is %d\n", num, factorial(num));
+
+    int fib_num = 5;  // Fibonacci calculation
+    printf("Fibonacci number at position %d is %d\n", fib_num, fibonacci(fib_num));
+    
+
+    printf("Hello, World!\n");
     return 0;
 }
